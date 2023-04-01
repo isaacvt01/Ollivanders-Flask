f837b8690d9e18ee7bcc86530dce9a11e1d4cc32
isaacvt01 <_vasqueztucent@cifpfbmoll.eu>
2023-04-01 19:58:01 +0200

    feat (templates, static, routes.py) Añadir ruta para mostrar en el navegador los items del inventario

--- routes/routes.py
+++ routes/routes.py
@@ -6,13 +6,13 @@ from database.database import DB
 bp = Blueprint('routes', __name__)
 
 # Se crea una instancia de la base de datos
-Db = DB()
+db = DB()
 
 
 # Ruta para mostrar los items de la base de datos
 @bp.route('/items', methods=['GET'])
 def get_all_items():
-    return Db.get_all_items()
+    return db.get_all_items()
 
 
 # Ruta para agregar un item a la base de datos
@@ -22,7 +22,7 @@ def add_item():
     sell_in = request.json['sell_in']
     quality = request.json['quality']
     type = request.json['type']
-    return Db.add_item(name, sell_in, quality, type)
+    return db.add_item(name, sell_in, quality, type)
 
 
 # Ruta para actualizar un item del inventario
@@ -32,37 +32,41 @@ def update_item():
     name = request.json['name']
     sell_in = request.json['sell_in']
     quality = request.json['quality']
-    return Db.update_item(item_id, name, sell_in, quality)
+    return db.update_item(item_id, name, sell_in, quality)
 
 
 # Ruta para mostrar un item
 @bp.route('/item/<item_id>', methods=['GET'])
 def get_item(item_id):
-    return Db.get_item(item_id)
+    return db.get_item(item_id)
 
 
 # Ruta para inicializar la base de datos
 @bp.route('/inicializar', methods=['POST'])
 def init_db():
-    return Db.init_db()
+    return db.init_db()
 
 
 @bp.route('/delete_item', methods=['DELETE'])
 def delete_item():
     item_id = request.json['_id']
-    return Db.delete_item(item_id)
+    return db.delete_item(item_id)
 
 
 @bp.route('/get_inventory', methods=['GET'])
 def get_inventory():
-    return Db.get_inventory()
+    return db.get_inventory()
 
 
 @bp.route('/update_inventory', methods=['PUT'])
 def update_inventory():
-    return Db.update_inventory()
+    return db.update_inventory()
 
 @bp.route('/delete_many', methods=['DELETE'])
 def delete_many_items():
     type = request.json.get('type')
-    return Db.delete_many_items(type)
+    return db.delete_many_items(type)
+
+@bp.route('/form/all_items', methods=['GET'])
+def all_items_html():
+    return render_template('all_items_temp.html', items=db.get_all_items().json['result'])
